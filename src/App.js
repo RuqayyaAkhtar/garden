3df74bb90cf9269839cb4aa9d4dd7700972451d7
RuqayyaAkhtar <112484548+RuqayyaAkhtar@users.noreply.github.com>
2026-03-02 23:26:02 +0500

    Update homepage and package.json

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Home from './component/home';
 import Nav from './component/Nav';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { HashRouter as Router } from 'react-router-dom';
 const App = () => {
   return (
     <BrowserRouter>
